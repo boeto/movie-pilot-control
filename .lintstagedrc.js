c2da28686022d73983b51a3fbfb5e0d7eac4d7c7
boeto <bo@eto.me>
2023-06-27 10:26:19 +0800

    feat: add Prettier to lintstagedrc.js for consistent code formatting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,9 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
+const buildPrettierCommand = 'prettier --ignore-unknown --write';
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*': [buildPrettierCommand],
 };
