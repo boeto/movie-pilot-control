b7dcbad32b097ed20d0d20ef9bb7e117f7f16898
boeto <bo@eto.me>
2023-07-07 16:36:34 +0800

    style: refactor DarkModeScript to improve readability and maintainability

--- src/components/atoms/DarkMode/DarkModeScript.tsx
+++ src/components/atoms/DarkMode/DarkModeScript.tsx
@@ -5,45 +5,45 @@ export const DarkModeScript: FC = () => (
     // eslint-disable-next-line react/no-danger
     dangerouslySetInnerHTML={{
       __html: /* html */ `
-            (function () {
-              function setMode(newMode) {
-                window.__mode = newMode;
-                if (newMode === 'dark') {
-                  document.documentElement.classList.add('dark');
-                } else if (newMode === 'light') {
-                  document.documentElement.classList.remove('dark');
-                }
-              }
-  
-              let preferredMode;
-              try {
-                preferredMode = localStorage.getItem('mode');
-              } catch (err) { }
-  
-              window.__setPreferredMode = function(newMode) {
-                preferredMode = newMode;
-                setMode(newMode);
-                try {
-                  localStorage.setItem('mode', newMode);
-                } catch (err) { }
-              };
-  
-              let initialMode = preferredMode;
-              let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  
-              if (!initialMode) {
-                initialMode = darkQuery.matches ? 'dark' : 'light';
-              }
-              setMode(initialMode);
-  
-              darkQuery.addEventListener('change', function (e) {
-                if (!preferredMode) {
-                  setMode(e.matches ? 'dark' : 'light');
-                }
-              });
-  
-            })();
-          `,
+        (function () {
+          function setMode(newMode) {
+            window.__mode = newMode;
+            if (newMode === 'dark') {
+              document.documentElement.classList.add('dark');
+            } else if (newMode === 'light') {
+              document.documentElement.classList.remove('dark');
+            }
+          }
+
+          let preferredMode;
+          try {
+            preferredMode = localStorage.getItem('mode');
+          } catch (err) { }
+
+          window.__setPreferredMode = function(newMode) {
+            preferredMode = newMode;
+            setMode(newMode);
+            try {
+              localStorage.setItem('mode', newMode);
+            } catch (err) { }
+          };
+
+          let initialMode = preferredMode;
+          let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+          if (!initialMode) {
+            initialMode = darkQuery.matches ? 'dark' : 'light';
+          }
+          setMode(initialMode);
+
+          darkQuery.addEventListener('change', function (e) {
+            if (!preferredMode) {
+              setMode(e.matches ? 'dark' : 'light');
+            }
+          });
+
+        })();
+      `,
     }}
   />
 );
